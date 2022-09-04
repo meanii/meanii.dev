51c098506972a29b9ea9deabbc07bd742dee7493
meanii <anilchauhan@mogiio.com>
2022-09-04 13:45:58 +0530

    add some projects cover

--- constant/projects.mjs
+++ constant/projects.mjs
@@ -3,42 +3,42 @@ export const PROJECTS = [
         name: `sync.ssh`,
         description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
         source: `https://github.com/meanii/sync.ssh`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        cover: `https://ik.imagekit.io/meanii/20220904_131635_t9NSo_GRd.png`,
         isNew: true
     },
     {
         name: `touka.ssh`,
         description: `touka.ssh is an ssh manager that allows you to manage all of your VPSs without keeping your password.`,
         source: `https://github.com/meanii/touka.ssh`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        cover: `https://ik.imagekit.io/meanii/20220904_132419_ELrz9MOlh.png`,
         isNew: true
     },
     {
         name: `stella`,
         description: `Stella - the first telegram group management bot to be built in Pyrogram.`,
         source: `https://github.com/SpookyGang/Stella`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        cover: `https://ik.imagekit.io/meanii/20220904_132535_s9SmRcBOR.png`,
         isNew: false
     },
     {
         name: `sessionMaker`,
         description: `a bot that can create session string of pyrogram and telethon.`,
         source: `https://github.com/meanii/sessionMaker`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        cover: `https://ik.imagekit.io/meanii/20220904_132646_ShQldEIDU.png`,
         isNew: false
     },
     {
         name: `search4`,
         description: `Search people on the Internet.`,
         source: `https://github.com/meanii/Search4`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        cover: `https://ik.imagekit.io/meanii/20220904_132733_DDwr7s8fn.png`,
         isNew: false
     },
     {
         name: `wallsDox`,
         description: `wallsdox Is platform to share high quality wallpapers for various platforms.`,
         source: `https://github.com/meanii/wallsDox`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
-        isNew: true
+        cover: `https://ik.imagekit.io/meanii/20220904_132837_2BpkUV5OW.png`,
+        isNew: false
     },
 ]
