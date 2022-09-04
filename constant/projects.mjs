5055741368208ba31c4e2d6cb0208a0b00a22ad8
meanii <anilchauhan@mogiio.com>
2022-09-04 12:13:39 +0530

    add some real projects

--- constant/projects.mjs
+++ constant/projects.mjs
@@ -7,37 +7,37 @@ export const PROJECTS = [
         isNew: true
     },
     {
-        name: `sync.ssh`,
-        description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
-        source: `https://github.com/meanii/sync.ssh`,
+        name: `touka.ssh`,
+        description: `touka.ssh is an ssh manager that allows you to manage all of your VPSs without keeping your password.`,
+        source: `https://github.com/meanii/touka.ssh`,
+        cover: `https://nextui.org/images/card-example-6.jpeg`,
+        isNew: true
+    },
+    {
+        name: `stella`,
+        description: `Stella - the first telegram group management bot to be built in Pyrogram.`,
+        source: `https://github.com/SpookyGang/Stella`,
         cover: `https://nextui.org/images/card-example-6.jpeg`,
         isNew: false
     },
     {
-        name: `sync.ssh`,
-        description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
-        source: `https://github.com/meanii/sync.ssh`,
+        name: `sessionMaker`,
+        description: `a bot that can create session string of pyrogram and telethon.`,
+        source: `https://github.com/meanii/sessionMaker`,
         cover: `https://nextui.org/images/card-example-6.jpeg`,
-        isNew: true
+        isNew: false
     },
     {
-        name: `sync.ssh`,
-        description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
-        source: `https://github.com/meanii/sync.ssh`,
+        name: `search4`,
+        description: `Search people on the Internet.`,
+        source: `https://github.com/meanii/Search4`,
         cover: `https://nextui.org/images/card-example-6.jpeg`,
-        isNew: true
+        isNew: false
     },
     {
-        name: `sync.ssh`,
-        description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
-        source: `https://github.com/meanii/sync.ssh`,
-        cover: `https://nextui.org/images/card-example-6.jpeg`,
-        isNew: true
-    },
-    {
-        name: `sync.ssh`,
-        description: `A powerful CLI tool built in golang that helps to sync your files/directories to your personal or private repository!`,
-        source: `https://github.com/meanii/sync.ssh`,
+        name: `wallsDox`,
+        description: `wallsdox Is platform to share high quality wallpapers for various platforms.`,
+        source: `https://github.com/meanii/wallsDox`,
         cover: `https://nextui.org/images/card-example-6.jpeg`,
         isNew: true
     },
