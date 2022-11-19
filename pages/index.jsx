518ecfe82fc93c41f9cc17074fb7b7f904213411
anil chauhan <anilchauhanxda@gmail.com>
2022-11-19 16:34:42 +0530

    💫 add some ogs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,40 @@
-import { Button, Text } from '@nextui-org/react';
+import { Text } from '@nextui-org/react';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/atoms/Layout';
 import Hero from '../components/hero/Hero';
 import Header from '../components/navbar/Header';
 import Projects from '../components/projects/Projects';
 import { Skills } from '../components/skills/Skills';
 import styles from '../styles/Home.module.css';
+import { Github } from '../utils/github';
 
 export default function Home() {
+    const [me, setMe] = useState(null);
+    useEffect(() => {
+        (async () => {
+            setMe(await new Github().me());
+        })();
+    }, []);
+
     return (
         <div>
+            <Head>
+                <meta property='og:url' content={`https://www.${me?.blog}`} />
+                <title>{me?.name}'s portfolio 🧋</title>
+                <link rel="shortcut icon" href={me?.avatar_url} />
+                <meta
+                    name='viewport'
+                    content='initial-scale=1.0, width=device-width'
+                />
+                <meta property='og:description' content={me?.bio} />
+                <meta property='og:image' content={me?.avatar_url} />
+                <meta
+                    property='og:site_name'
+                    content={`${me?.name}'s portfolio`}
+                />
+            </Head>
+
             <Layout css={{ m: 6 }}>
                 {/* init main components */}
                 <Header />
